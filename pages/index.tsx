5a1f16fb2f2fca7d1ddc75c4ae47feb82b130242
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-04-09 06:29:16 +0900

    TailwindCSS setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-  return null;
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-black">it works</h1>
+    </div>
+  );
 }
