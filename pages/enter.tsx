bbeb7714b3b58b62cf1307b9209b9d08e3a3e492
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-04-13 02:48:50 +0900

    login page clear

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -41,13 +41,14 @@ export default function Enter() {
           </div>
         </div>
         <form className="flex flex-col mt-8">
-          <label className="text-sm font-medium text-gray-700">
+          <label htmlFor="input" className="text-sm font-medium text-gray-700">
             {method === "email" ? "Email address" : null}
             {method === "phone" ? "Phone number" : null}
           </label>
           <div className="mt-1">
             {method === "email" ? (
               <input
+                id="input"
                 type="email"
                 className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                 required
@@ -59,6 +60,7 @@ export default function Enter() {
                   +82
                 </span>
                 <input
+                  id="input"
                   type="number"
                   className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                   required
@@ -94,7 +96,7 @@ export default function Enter() {
             <button className="flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
               <svg
                 className="w-5 h-5"
-                aria-aria-hidden="true"
+                aria-hidden="true"
                 fill="currentColor"
                 viewBox="0 0 20 20"
               >
