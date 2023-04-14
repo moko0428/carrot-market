d2caa0342f57f56070b87b237e294a03d72c14a8
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-04-14 09:03:07 +0900

    community files, Update to Link, Layout

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -1,15 +1,18 @@
 import type { NextPage } from "next";
+import Layout from "../../components/layout";
+import TextArea from "../../components/textarea";
 
 const CommunityPostDetail: NextPage = () => {
   return (
-    <div>
+    <Layout canGoBack>
       <span className="inline-flex my-3 ml-4 items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
         동네질문
       </span>
       <div className="flex mb-3 px-4 cursor-pointer pb-3  border-b items-center space-x-3">
         <div className="w-10 h-10 rounded-full bg-slate-300" />
         <div>
-          <p className="text-sm font-medium text-gray-700">Moko Lee</p>
+          {" "}
+          <p className="text-sm font-medium text-gray-700">Steve Jebs</p>
           <p className="text-xs font-medium text-gray-500">
             View profile &rarr;
           </p>
@@ -18,7 +21,7 @@ const CommunityPostDetail: NextPage = () => {
       <div>
         <div className="mt-2 px-4 text-gray-700">
           <span className="text-orange-500 font-medium">Q.</span> What is the
-          best MealKit store?
+          best mandu restaurant?
         </div>
         <div className="flex px-4 space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-[2px]  w-full">
           <span className="flex space-x-2 items-center text-sm">
@@ -62,26 +65,26 @@ const CommunityPostDetail: NextPage = () => {
           <div className="w-8 h-8 bg-slate-200 rounded-full" />
           <div>
             <span className="text-sm block font-medium text-gray-700">
-              준영
+              Steve Jebs
             </span>
             <span className="text-xs text-gray-500 block ">2시간 전</span>
             <p className="text-gray-700 mt-2">
-              The best MealKit store is the one next to my house.
+              The best mandu restaurant is the one next to my house.
             </p>
           </div>
         </div>
       </div>
       <div className="px-4">
-        <textarea
-          className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500 "
-          rows={4}
+        <TextArea
+          name="description"
           placeholder="Answer this question!"
+          required
         />
         <button className="mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none ">
           Reply
         </button>
       </div>
-    </div>
+    </Layout>
   );
 };
 
