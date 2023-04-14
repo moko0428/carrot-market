b8c12acdadc95874eea3cfbcf7d13107b9067211
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-04-14 09:01:14 +0900

    fix: Chat index, Update to Link

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,24 +1,24 @@
 import { NextPage } from "next";
 
 import Layout from "../../components/layout";
+import Link from "next/link";
 
 const Chats: NextPage = () => {
   return (
     <Layout hasTabBar title="채팅">
       <div className="py-10 divide-y-[1px]">
         {[1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-          <div
-            key={i}
-            className="flex mb-3  px-4 cursor-pointer pb-3 items-center space-x-3"
-          >
-            <div className="w-10 h-10 rounded-full bg-slate-300" />
-            <div>
-              <p className=" text-gray-700">Moko Lee</p>
-              <p className="text-sm -gray-500">
-                See you tomarrow in the corner at 2pm!
-              </p>
-            </div>
-          </div>
+          <Link legacyBehavior href={`/chats/${i}`} key={i}>
+            <a className="flex px-4 cursor-pointer py-3 items-center space-x-3">
+              <div className="w-12 h-12 rounded-full bg-slate-300" />
+              <div>
+                <p className="text-gray-700">Steve Jebs</p>
+                <p className="text-sm  text-gray-500">
+                  See you tomorrow in the corner at 2pm!
+                </p>
+              </div>
+            </a>
+          </Link>
         ))}
       </div>
     </Layout>
