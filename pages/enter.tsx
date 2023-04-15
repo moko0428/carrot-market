455f66b1fd0ba2a7756f5be6dd9010c81d6668a3
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-04-16 04:02:38 +0900

    clean Code

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { cls } from "../libs/utils";
+import { cls } from "../libs/client/utils";
 import Input from "../components/input";
 import Button from "../components/button";
 import { NextPage } from "next";
 import { useForm } from "react-hook-form";
 import { forEachTrailingCommentRange } from "typescript";
+import useMutation from "../libs/client/useMutation";
 
 interface EnterForm {
   email?: string;
@@ -12,6 +13,7 @@ interface EnterForm {
 }
 
 const Enter: NextPage = () => {
+  const [enter, { loading, data, error }] = useMutation("/api/users/enter");
   const [submitting, setSubmitting] = useState(false);
   const { register, handleSubmit, reset } = useForm<EnterForm>();
   const [method, setMethod] = useState<"email" | "phone">("email");
@@ -24,16 +26,7 @@ const Enter: NextPage = () => {
     setMethod("phone");
   };
   const onValid = (data: EnterForm) => {
-    setSubmitting(true);
-    fetch("/api/users/enter", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then(() => {
-      setSubmitting(false);
-    });
+    enter(data);
   };
   return (
     <div className="mt-16 px-4">
