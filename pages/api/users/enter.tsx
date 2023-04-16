354d14791b3e0f41a3faa9adb5dbd8aef1ce6424
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-04-17 08:41:56 +0900

    create: 토큰 로직

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -5,18 +5,25 @@ import { Prisma } from "@prisma/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
-  const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  const user = phone ? { phone: +phone } : { email };
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
     },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
   });
-  console.log(user);
+  console.log(token);
   // if (email) {
   //   user = await client.user.findUnique({
   //     where: {
