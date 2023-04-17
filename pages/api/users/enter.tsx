95f2e061ea4b05b0c2845e2925e4aed0db517e6e
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-04-18 08:44:54 +0900

    delete: Twilio 삭제

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,16 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
-import twilio from "twilio";
 
-const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWLIO_TOKEN);
+import smtpTransport from "@libs/server/email";
 
-async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseType>
-) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
-  const user = phone ? { phone: +phone } : email ? { email } : null;
+  const user = phone ? { phone: +phone } : { email };
   if (!user) return res.status(400).json({ ok: false });
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
@@ -29,19 +25,29 @@ async function handler(
       },
     },
   });
-  if (phone) {
-    const message = await twilioClient.messages.create({
-      messagingServiceSid: process.env.TWILIO_MSID,
-      to: process.env.MY_PHONE! as string,
-      body: `Your login token is ${payload}.`,
-    });
-    console.log(message);
+  console.log(token);
+  if (email) {
+    const mailOptions = {
+      from: process.env.MAIL_ID,
+      to: email,
+      subject: "Carrot Authentication Email",
+      text: `Authentication Code : ${payload}`,
+    };
+    const result = await smtpTransport.sendMail(
+      mailOptions,
+      (error, responses) => {
+        if (error) {
+          console.log(error);
+          return null;
+        } else {
+          console.log(responses);
+          return null;
+        }
+      }
+    );
+    smtpTransport.close();
+    console.log(result);
   }
-
-  return res.json({
-    ok: true,
-  });
-
   return res.status(200).end();
 }
 
