4f7dc01d697b93ee9049b10ff31e83b7e28d9567
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-05-13 19:02:00 +0900

    fix: comments nextauth files, _app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,7 @@ import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig
-      value={{
-        fetcher: (url: string) =>
-          fetch(url).then((response) => response.json()),
-      }}
-    >
+    <SWRConfig>
       <div className="w-full max-w-xl mx-auto">
         <Component {...pageProps} />
       </div>
