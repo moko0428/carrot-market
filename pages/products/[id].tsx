642cdefa97dd2b69d45a1c2ef21f5d05c449dafb
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-05-15 17:53:25 +0900

    Related Products

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -2,8 +2,27 @@ import type { NextPage } from "next";
 import Link from "next/link";
 import Layout from "../../components/layout";
 import Button from "../../components/button";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import { Product, User } from "@prisma/client";
+import { BooleanLiteral } from "typescript";
+import products from "pages/api/products";
+
+interface ProductWithUser extends Product {
+  user: User;
+}
+
+interface ItemDetailResponse {
+  ok: Boolean;
+  product: ProductWithUser;
+  relatedProducts: Product[];
+}
 
 const ItemDetail: NextPage = () => {
+  const router = useRouter();
+  const { data } = useSWR<ItemDetailResponse>(
+    router.query.id ? `/api/products/${router.query.id}` : null
+  );
   return (
     <Layout canGoBack>
       <div className="px-5 py-4">
@@ -12,27 +31,27 @@ const ItemDetail: NextPage = () => {
           <div className="flex cursor-pointer py-3  border-t border-b items-center space-x-3">
             <div className="w-12 h-12 rounded-full bg-slate-300" />
             <div>
-              <p className="text-sm font-medium text-gray-700">Moko Lee</p>
-              <p className="text-sm font-medium text-gray-500">
-                View profile &rarr;
+              <p className="text-sm font-medium text-gray-700">
+                {data?.product?.user?.name}
               </p>
+              <Link
+                legacyBehavior
+                href={`/users/profiles/${data?.product?.user?.id}`}
+              >
+                <a className="test-xs font-medium text-gray-500">
+                  View profile $rarr;
+                </a>
+              </Link>
             </div>
           </div>
-          <div className="mt-10">
+          <div className="mt-5">
             <h1 className="text-3xl font-bold text-gray-900">
-              Galaxy S10 Note Ultra
+              {data?.product?.name}
             </h1>
-            <span className="text-3xl mt-3 text-gray-900">$140</span>
-            <p className="text-base my-6 text-gray-700">
-              My money&apos;s in that office, right? If she start giving me some
-              bullshit about it ain&apos;t there, and we got to go someplace
-              else and get it, I&apos;m gonna shoot you in the head then and
-              there. Then I&apos;m gonna shoot that bitch in the kneecaps, find
-              out where my goddamn money is. She gonna tell me too. Hey, look at
-              me when I&apos;m talking to you, motherfucker. You listen: we go
-              in there, and that ni**a Winston or anybody else is in there, you
-              the first motherfucker to get shot. You understand?
-            </p>
+            <span className="text-2xl block mt-3 text-gray-900">
+              ${data?.product?.price}
+            </span>
+            <p className="my-6 to-gray-700">{data?.product?.description}</p>
             <div className="flex items-center justify-between space-x-2">
               <Button large text="Talk to seller" />
               <button className="p-3 border-none rounded-md flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500">
@@ -58,11 +77,13 @@ const ItemDetail: NextPage = () => {
         <div>
           <h2 className="text-2xl font-bold text-gray-900">Similar items</h2>
           <div className="mt-6 grid grid-cols-2 gap-4">
-            {[1, 2, 3, 4, 5, 6].map((_, i) => (
-              <div key={i}>
+            {data?.relatedProducts.map((product) => (
+              <div key={product.id}>
                 <div className="h-56 w-full mb-4 bg-slate-300" />
-                <h3 className="text-gray-700 mb-1">Galaxy S10 Note Ultra</h3>
-                <p className="text-sm font-medium text-gray-900">$6</p>
+                <h3 className="text-gray-700 mb-1">{product.name}</h3>
+                <p className="text-sm font-medium text-gray-900">
+                  ${product.price}
+                </p>
               </div>
             ))}
           </div>
