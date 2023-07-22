213ffb83ee92a80e605dc4fa3b0f07596cb0ffbc
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-07-22 22:30:40 +0900

    위치 추가

---
+++ pages/api/posts/index.ts
@@ -0,0 +1,75 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  if (req.method === "POST") {
+    const {
+      body: { question, latitude, longitude },
+      session: { user },
+    } = req;
+    const post = await client.post.create({
+      data: {
+        question,
+        latitude,
+        longitude,
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      },
+    });
+    res.json({
+      ok: true,
+      post,
+    });
+  }
+  if (req.method === "GET") {
+    const {
+      body: { question, latitude, longitude },
+      session: { user },
+    } = req;
+    const parsedLatitude = parseFloat(latitude.toString());
+    const parsedLongitude = parseFloat(longitude.toString());
+    const posts = await client.post.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            avatar: true,
+          },
+        },
+        _count: {
+          select: {
+            wondering: true,
+            answers: true,
+          },
+        },
+      },
+      where: {
+        latitude: {
+          gte: parsedLatitude - 0.01,
+          lte: parsedLongitude + 0.01,
+        },
+        longitude: {
+          gte: parsedLatitude - 0.01,
+          lte: parsedLongitude - 0.01,
+        },
+      },
+    });
+    res.json({
+      ok: true,
+      posts,
+    });
+  }
+}
+
+export default withApiSession(
+  withHandler({ methods: ["GET", "POST"], handler })
+);
